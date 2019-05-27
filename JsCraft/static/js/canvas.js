64911e638b8c2715de36060dedb828965aac5d3e
spotandjake <40705786+spotandjake@users.noreply.github.com>
2019-05-27 02:23:53 -0400

    added all files to repository

--- JsCraft/static/js/canvas.js
+++
@@ -1,7 +0,0 @@
-var context = document.getElementById('materialselector2').getContext("2d");
-	
-var img = new Image();
-img.onload = function () {
-    context.drawImage(img, 0, 0);
-}
-img.src = "./media/image.png";
