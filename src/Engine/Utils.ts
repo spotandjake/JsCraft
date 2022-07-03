33830796c7316602f420e9e3ba5afd3c35a466ed
Spotandjake <spotandjake@hotmail.com>
2022-07-03 17:43:24 -0400

    Performance Improvments

--- src/Engine/Utils.ts
+++ src/Engine/Utils.ts
@@ -6,23 +6,40 @@ export const clamp = (n: number, min: number, max: number): number =>
   Math.min(Math.max(n, min), max);
 // Meshing Helpers
 export const pushQuad = (v: number[], p1: number[], p2: number[], p3: number[], p4: number[]) => {
-  // 1160
-  const lenA = v.length;
+  // Cached Array Lengths
+  const vLength = v.length;
+  const p1Length = p1.length;
+  const p2Length = p2.length;
+  const p3Length = p3.length;
+  const p4Length = p4.length;
+  // Lengths At State
+  const lenA = vLength;
+  const lenB = vLength + p1Length;
+  const lenC = lenB + p2Length;
+  const lenD = lenC + p3Length;
+  const lenE = lenD + p3Length;
+  const lenF = lenE + p4Length;
+  // Set The Array Length To The Proper Size
+  v.length = vLength + p1Length + p2Length + p3Length + p3Length + p4Length + p1Length;
+  // Fill The Array
   let i = 0;
-  for (i = 0; i < p1.length; i++) {
+  for (i = 0; i < p1Length; ++i) {
     v[lenA + i] = p1[i];
   }
-  const lenB = v.length;
-  for (i = 0; i < p2.length; i++) {
+  for (i = 0; i < p2Length; ++i) {
     v[lenB + i] = p2[i];
   }
-  const lenC = v.length;
-  for (i = 0; i < p3.length; i++) {
+  for (i = 0; i < p3Length; ++i) {
     v[lenC + i] = p3[i];
   }
-  const lenD = v.length;
-  for (i = 0; i < p4.length; i++) {
-    v[lenD + i] = p4[i];
+  for (i = 0; i < p3Length; ++i) {
+    v[lenD + i] = p3[i];
+  }
+  for (i = 0; i < p4Length; ++i) {
+    v[lenE + i] = p4[i];
+  }
+  for (i = 0; i < p1Length; ++i) {
+    v[lenF + i] = p1[i];
   }
   return v;
 };
--- src/Engine/World/Chunk.ts
+++ src/Engine/World/Chunk.ts
@@ -2,7 +2,7 @@ import Block from './Block';
 import { Mesh, BlockType, BlockDirection } from '../Types';
 import Renderer from '../Renderer';
 class Chunk {
-  private blocks: Block[][][];
+  private blocks: Block[];
   private size: number;
   private mesh: Mesh | undefined = undefined;
   private x: number;
@@ -14,13 +14,7 @@ class Chunk {
     this.y = y;
     this.z = z;
     // Create Block Array
-    this.blocks = [];
-    for (let x = 0; x < size; x++) {
-      this.blocks[x] = [];
-      for (let y = 0; y < size; y++) {
-        this.blocks[x][y] = [];
-      }
-    }
+    this.blocks = new Array(this.size * this.size * this.size);
   }
   // Public Api
   public getBlock(x: number, y: number, z: number): Block {
@@ -28,7 +22,7 @@ class Chunk {
     if (x < 0 || y < 0 || z < 0 || x >= this.size || y >= this.size || z >= this.size)
       throw `Block(x: ${x}, y: ${y}): Cannot Get Block out Of Chunk Bounds`;
     // Try To Get The Block
-    const block = this.blocks[x][y][z];
+    const block = this.blocks[x + y * this.size + z * this.size];
     if (block === undefined) {
       // Return An Air Block By Default
       // TODO: Make This An Air Block
@@ -43,53 +37,59 @@ class Chunk {
     if (x < 0 || y < 0 || z < 0 || x >= this.size || y >= this.size || z >= this.size)
       throw `Block(x: ${x}, y: ${y}): Cannot Set Block out Of Chunk Bounds`;
     // Set The Block
-    this.blocks[x][y][z] = block;
+    this.blocks[x + y * this.size + z * this.size] = block;
     // Clear The Mesh Cache
     this.mesh = undefined;
   }
-  private _getBlock(x: number, y: number, z: number): Block | { blockType: BlockType.Air } {
-    // If Block Is Out Of Bounds Throw Error
-    if (x < 0 || y < 0 || z < 0 || x >= this.size || y >= this.size || z >= this.size)
-      return { blockType: BlockType.Air };
+  private _getBlock(x: number, y: number, z: number): Block | undefined {
     // Try To Get The Block
-    const block = this.blocks[x][y][z];
+    const block = this.blocks[x + y * this.size + z * this.size];
+    return block;
+  }
+  private canSeeThrough(x: number, y: number, z: number): boolean {
+    // If Block Is Out Of Bounds Throw Error
+    if (x < 0 || y < 0 || z < 0 || x >= this.size || y >= this.size || z >= this.size) return true;
+    // Try To Get The Block
+    const block = this.blocks[x + y * this.size + z * this.size];
     if (block === undefined) {
-      return { blockType: BlockType.Air };
+      return true;
     } else {
-      return block;
+      return block.blockType === BlockType.Air;
     }
   }
   public render(renderer: Renderer): Mesh {
     // Generate Mesh Data
     if (this.mesh === undefined) {
+      // Create A Chunk mesh with a guess size to try to avoid reallocations based on pushQuad
       const chunkMesh: number[] = [];
       // Generate The Meshes For All The Cubes
-      for (let x = 0; x < this.size; x++) {
-        for (let y = 0; y < this.size; y++) {
-          for (let z = 0; z < this.size; z++) {
+      const chunkSize = this.size;
+      for (let x = 0; x < chunkSize; x++) {
+        for (let y = 0; y < chunkSize; y++) {
+          for (let z = 0; z < chunkSize; z++) {
             // Render The Block Mesh
             const block = this._getBlock(x, y, z);
-            if (block.blockType === BlockType.Air) {
+            if (block === undefined) {
               continue;
             }
             // Determine Which Sides Are Open
             // TODO: Define Open Block
-            if (this._getBlock(x, y + 1, z).blockType === BlockType.Air) {
+            if (this.canSeeThrough(x, y + 1, z)) {
               block.render(BlockDirection.Up, chunkMesh);
             }
-            if (this._getBlock(x, y - 1, z).blockType === BlockType.Air) {
+            if (this.canSeeThrough(x, y - 1, z)) {
               block.render(BlockDirection.Down, chunkMesh);
             }
-            if (this._getBlock(x, y, z + 1).blockType === BlockType.Air) {
+            if (this.canSeeThrough(x, y, z + 1)) {
               block.render(BlockDirection.South, chunkMesh);
             }
-            if (this._getBlock(x, y, z - 1).blockType === BlockType.Air) {
+            if (this.canSeeThrough(x, y, z - 1)) {
               block.render(BlockDirection.North, chunkMesh);
             }
-            if (this._getBlock(x + 1, y, z).blockType === BlockType.Air) {
+            if (this.canSeeThrough(x + 1, y, z)) {
               block.render(BlockDirection.East, chunkMesh);
             }
-            if (this._getBlock(x - 1, y, z).blockType === BlockType.Air) {
+            if (this.canSeeThrough(x - 1, y, z)) {
               block.render(BlockDirection.West, chunkMesh);
             }
             // TODO: If The Quad is not The Full Width Determine If We Need To Show The Other Sides
@@ -101,6 +101,7 @@ class Chunk {
       const gl = renderer.gl;
       const buffer = gl.createBuffer()!;
       gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+      // TODO: This takes 1.5 seconds a alone
       const _chunkMesh = new Float32Array(chunkMesh);
       gl.bufferData(gl.ARRAY_BUFFER, _chunkMesh, gl.STATIC_DRAW);
       // Cache The Mesh
